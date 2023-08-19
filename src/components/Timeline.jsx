4d33d37d44c2a367ebc646a7c2ced1dd4e12b28e
Ilir <ilir.hajdari111@gmail.com>
2023-08-19 14:45:03 -0400

    imported Header

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,10 +1,11 @@
 import timeline from "../data/timeline";
 import TimelineItem from "./TimelineItem";
-
+import Header from "./Header";
 function Timeline() {
   return (
     <div className="flex flex-col md:flex-row justify-center my-20">
       <div className="w-full md:w-7/12">
+        <Header>Timeline</Header>
         {timeline.map((item, index) => (
           <TimelineItem
             key={index}
