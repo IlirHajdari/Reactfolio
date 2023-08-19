f9c4ebd9c447b976570a28b37727e0c29e957ee9
Ilir <ilir.hajdari111@gmail.com>
2023-08-19 15:42:40 -0400

    configured dark mode text

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable react/prop-types */
 function PortfolioItem({ imgUrl, title, stack }) {
   return (
-    <div className="border-2 border-stone-900 rounded-md overflow-hidden">
+    <div className="border-2 border-stone-900 rounded-md dark:border-white overflow-hidden">
       <img
         src={imgUrl}
         alt="portfolio"
         className="w-full h-36 md:h-48 object-cover cursor-pointer"
       />
       <div className="w-full p-4">
-        <h3 className="text-large md:text-xl mb-2 md:mb-3 font-semibold text-center">
+        <h3 className="text-large md:text-xl mb-2 md:mb-3 font-semibold text-center dark:text-white">
           {title}
         </h3>
-        <p className="flex felx-wrap gap-2 flex-row items-center justify-center text-xs md:text-sm ">
+        <p className="flex felx-wrap gap-2 flex-row items-center justify-center text-xs md:text-sm dark:text-white ">
           {stack.map((item, index) => (
             <span
               key={index}
-              className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md"
+              className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md"
             >
               {item}
             </span>
