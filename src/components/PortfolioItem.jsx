d06b33a1e87546f5a171b9fea475dff221e2cd89
Ilir <ilir.hajdari111@gmail.com>
2023-08-18 22:48:51 -0400

    updated Portfolio.jsx, PortfolioItem.jsx, added thumbnail images to projects in portfolio.js, and moved assets folder to public folder for production

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable react/prop-types */
 function PortfolioItem({ imgUrl, title, stack }) {
   return (
-    <div>
+    <div className="border-2 border-stone-900 rounded-md overflow-hidden">
       <img
         src={imgUrl}
         alt="portfolio"
-        className="w-full h-26 md:h-48 object-cover cursor-pointer"
+        className="w-full h-36 md:h-48 object-cover cursor-pointer"
       />
       <div className="w-full p-4">
-        <h3 className="text-large md:text-xl mb-2 md:mb-3 font-semibold">
+        <h3 className="text-large md:text-xl mb-2 md:mb-3 font-semibold text-center">
           {title}
         </h3>
-        <p className="flex felx-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm ">
+        <p className="flex felx-wrap gap-2 flex-row items-center justify-center text-xs md:text-sm ">
           {stack.map((item, index) => (
             <span
               key={index}
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
   {
     title: "CitySnap",
-    imgUrl: "",
+    imgUrl: "/assets/CitySnap Image.jpg",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://ilirhajdari.github.io/city-snap/",
   },
   {
     title: "Your Daily Weather",
-    imgUrl: "",
+    imgUrl: "/assets/WeatherDashSS.png",
     stack: ["HTML", "CSS", "JavaScript"],
     link: "https://ilirhajdari.github.io/your-daily-weather/",
   },
   {
     title: "Jotter: Electronic Document Interface",
-    imgUrl: "",
+    imgUrl: "/assets/JEDI.png",
     stack: ["Express.js", "Webpack", "Babel", "HTML", "CSS"],
     link: "https://j-e-d-i-a588d6870c22.herokuapp.com/",
   },
