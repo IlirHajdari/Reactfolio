98dae2d5c01b47f999c179de4bef76aef5e11597
Ilir <ilir.hajdari111@gmail.com>
2023-08-18 21:11:52 -0400

    made some tailwinds styling changes and added intro info

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,11 @@
 function Intro() {
-  return <div>Intro</div>;
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Ilir</h1>
+      <p>Systems Administrator & Web Developer</p>
+      <p>This is my entire bio.</p>
+    </div>
+  );
 }
 
 export default Intro;
