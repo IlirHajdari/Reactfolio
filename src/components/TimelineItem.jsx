cb2540c2eb550a60f5d08142464fae48341cc749
Ilir <ilir.hajdari111@gmail.com>
2023-08-19 00:51:06 -0400

    made changes to timeline and timelineitem files in component folder. Made changes to timeline file in data folder.

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -1,8 +1,8 @@
 function TimelineItem({ year, title, duration, details }) {
   return (
-    <ol className="flex flex-col med-flex-row relative border-l border-stone-200">
+    <ol className="flex flex-col med-flex-row relative border-l border-stone-400">
       <li className="mb-10 ml-4">
-        <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-white"></div>
+        <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border border-black"></div>
         <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
           <span className="inilne-block px-2 py-1 font-semibold text-white bg-stone-900 rounded-md">
             {year}
