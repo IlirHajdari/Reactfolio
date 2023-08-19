1c92904f2355268ec3be6f5fc109aedcf5a36ea0
Ilir <ilir.hajdari111@gmail.com>
2023-08-18 23:24:03 -0400

    added the needed code within the file

---
+++ src/data/timeline.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    year: "Mar.2023",
+    title: "Started Web Development Bootcamp",
+    duration: "6 months",
+    details: `Immediately after graduating college, I enrolled in a 6-month Full Stack Web Development Bootcamp.
+        I delved deep into the intricacies of both front-end and back-end web development. 
+        The curriculum covered a comprehensive range of technologies including HTML, CSS, JavaScript, React, Node.js, and relational databases. 
+        By the end of the bootcamp, which is at the end of this month, August, I had built multiple projects, 
+        showcasing my ability to create full-fledged web applications from scratch. 
+        This hands-on experience complemented my theoretical knowledge from college, 
+        equipping me with the practical skills needed in the real-world development environment.`,
+  },
+  {
+    year: "Feb.2023",
+    title: "Bachelor's Degree in Computer Science",
+    duration: "4 years",
+    details: `I graduated from college with a BS in Computer Science in February of this year!
+      I learned about computer science theory, architecture, programming and delved into artificial intelligence and facial recognition.`,
+  },
+];
