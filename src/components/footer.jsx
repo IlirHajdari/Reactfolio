a3c6de6234f26b41fc4ebdef978935f46d1a2433
Ilir <ilir.hajdari111@gmail.com>
2023-08-19 13:09:55 -0400

    Committing changes to footer.jsx.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,12 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="py-5 text-enter">
+      {/* Social Icons */}
+      <p className="text-sm mt-2 opacity-50">
+        &copy; {new Date().getFullYear()} Ilir Hajdari. All right reserved.
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
