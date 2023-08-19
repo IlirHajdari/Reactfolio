c33548f0071d4d955978ec8abb04042c27834b14
Ilir <ilir.hajdari111@gmail.com>
2023-08-18 21:54:54 -0400

    added needed code to Portfolio function

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,22 @@
+import portfolioData from "../data/portfolio";
+import PortfolioItem from "./PortfolioItem";
+
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div className="flex flex-col med:flex-row items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {portfolioData.map((project, index) => (
+          <PortfolioItem
+            key={index}
+            imgUrl={project.imgUrl}
+            title={project.title}
+            stack={project.stack}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
