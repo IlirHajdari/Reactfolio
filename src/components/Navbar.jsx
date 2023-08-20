9062070ca91d53b31cd41cd53a64d88c175a61fa
Ilir <ilir.hajdari111@gmail.com>
2023-08-19 23:05:22 -0400

    changed navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,41 @@
 import navLinks from "../data/navbar";
 
+const styles = {
+  "About Me": {
+    base: "bg-red-800 hover:bg-red-700 dark: bg-red-200 dark:text-white",
+    text: "text-black",
+  },
+  Portfolio: {
+    base: "bg-green-800 hover:bg-green-700 dark:text-white",
+    text: "text-black",
+  },
+  Contact: {
+    base: "bg-blue-800 hover:bg-blue-700 dark:text-white",
+    text: "text-black",
+  },
+  Resume: {
+    base: "bg-yellow-600 hover:bg-yellow-500 dark:text-white",
+    text: "text-black",
+  },
+};
+
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-5">
-      {navLinks.map((link, index) => (
-        <a key={index} href={link.link} className="mr-4">
-          {link.title}
-        </a>
-      ))}
+    <nav className="flex justify-center items-center p-5">
+      <ul className="flex space-x-4">
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <a
+              href={link.link}
+              className={`p-2 py-2 px-5 rounded transition-colors ${
+                styles[link.title].base
+              } ${styles[link.title].text}`}
+            >
+              {link.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
