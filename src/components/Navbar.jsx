0d62461a1eb27ecd4c1be7aaf551ad57cd788efc
Ilir <ilir.hajdari111@gmail.com>
2023-08-20 00:00:41 -0400

    fixed flex issues with navbar, corrected navbar color style, and added 3 more projects to showcase in portfolio.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,55 @@
+import { useState } from "react";
 import navLinks from "../data/navbar";
 
 const styles = {
   "About Me": {
-    base: "bg-red-800 hover:bg-red-700 dark: bg-red-200 dark:text-white",
-    text: "text-black",
+    base: "bg-red-500 hover:bg-red-700 dark:bg-red-600 dark:hover:bg-red-700",
+    text: "text-black dark:text-white",
   },
   Portfolio: {
-    base: "bg-green-800 hover:bg-green-700 dark:text-white",
-    text: "text-black",
+    base: "bg-green-500 hover:bg-green-700 dark:bg-green-600 dark:hover:bg-green-700",
+    text: "text-black dark:text-white",
   },
   Contact: {
-    base: "bg-blue-800 hover:bg-blue-700 dark:text-white",
-    text: "text-black",
+    base: "bg-blue-500 hover:bg-blue-700 dark:bg-blue-600 dark:hover:bg-blue-700",
+    text: "text-black dark:text-white",
   },
   Resume: {
-    base: "bg-yellow-600 hover:bg-yellow-500 dark:text-white",
-    text: "text-black",
+    base: "bg-yellow-500 hover:bg-yellow-500 dark:bg-yellow-600 dark:hover:bg-yellow-500",
+    text: "text-black dark:text-white",
   },
 };
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="flex justify-center items-center p-5">
-      <ul className="flex space-x-4">
+    <nav className="flex justify-center items-center p-5 w-full relative">
+      <div
+        className="absolute top-4 left-4 md:hidden cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        ☰
+      </div>
+
+      <ul
+        className={`flex space-x-4 ${
+          isOpen ? "flex-col space-y-4 items-center" : "flex-row"
+        } 
+          md:flex-row md:space-y-0 md:items-center md:justify-center transition-all duration-300`}
+      >
         {navLinks.map((link, index) => (
-          <li key={index}>
+          <li
+            key={index}
+            className={`transition-transform duration-300 transform ${
+              isOpen ? "scale-100" : "scale-0 md:scale-100"
+            } ${link.title === "About Me" ? "pl-4 md:pl-0" : ""}`}
+          >
             <a
               href={link.link}
-              className={`p-2 py-2 px-5 rounded transition-colors ${
-                styles[link.title].base
-              } ${styles[link.title].text}`}
+              className={`p-2 py-2 px-5 rounded transition-colors 
+      ${styles[link.title].base} ${styles[link.title].text}`}
+              onClick={() => setIsOpen(false)}
             >
               {link.title}
             </a>
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -17,4 +17,22 @@ export default [
     stack: ["Express.js", "Webpack", "HTML", "CSS"],
     link: "https://j-e-d-i-a588d6870c22.herokuapp.com/",
   },
+  {
+    title: "Work Day Scheduler",
+    imgUrl: "/assets/uiImage.png",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://ilirhajdari.github.io/schedule-your-work/",
+  },
+  {
+    title: "Jot Box",
+    imgUrl: "/assets/jot-box_image.png",
+    stack: ["HTML", "CSS", "Express.js", "MySQL"],
+    link: "",
+  },
+  {
+    title: "JavaScript Knowledge Quiz",
+    imgUrl: "/assets/image.png",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://ilirhajdari.github.io/javascript-knowledge-quiz/",
+  },
 ];
